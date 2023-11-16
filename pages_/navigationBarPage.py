0f4eb48ce6723088d8aedfe306b5cde6fed2a525
JenyaKirakosyan27 <147413285+JenyaKirakosyan27@users.noreply.github.com>
2023-11-16 15:45:00 +0400

    Update navigationBarPage.py

--- pages_/navigationBarPage.py
+++ pages_/navigationBarPage.py
@@ -7,7 +7,7 @@ class NavigationBar(BasePage):
     def __init__(self, driver: webdriver.Chrome):
         super().__init__(driver)
         self.__cartButtonLocator = (By.ID, "nav-cart-count-container")
-        self.__textFieldLocator = (By.NAME, "field-keywords")
+        self.__productNameFieldLocator = (By.NAME, "field-keywords")
         self.__searchButtonLocator = (By.ID, "nav-search-submit-button")
 
     def click_to_cart_button(self):
@@ -15,8 +15,8 @@ class NavigationBar(BasePage):
         self._click_to_element(cartButtonElement)
 
     def fill_search_field(self, searchElement):
-        textFieldElement = self._find_element(self.__textFieldLocator)
-        self._fill_field(textFieldElement, searchElement)
+        productNameFieldElement = self._find_element(self.__productNameFieldLocator)
+        self._fill_field(productNameFieldElement, searchElement)
 
     def click_to_search_button(self):
         searchButtonElement = self._find_element(self.__searchButtonLocator)
