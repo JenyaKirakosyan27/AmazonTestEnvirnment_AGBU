efccd379a8ee871dd0b6b6d4efc4a8f35bceff9f
JenyaKirakosyan27 <147413285+JenyaKirakosyan27@users.noreply.github.com>
2023-11-16 14:43:48 +0400

    Update cartPage.py

--- pages_/cartPage.py
+++ pages_/cartPage.py
@@ -9,9 +9,11 @@ class CartPage(BasePage):
         self.__firstProductDeleteButtonLocator = (By.XPATH, "(//input[@value = 'Delete'])[1]")
         self.__firstProductSaveForLaterButtonLocator = (By.XPATH, "(//input[@value='Save for later'])[1]")
         self.__allItemsDeselectFromCartLocator = (By.ID, "deselect-all")
+        self.__cartCountLocator = (By.ID, "nav-cart-count")
+
     def delete_first_product_from_cart(self):
-       firstProductDeleteButtonElement = self._find_element(self.__firstProductDeleteButtonLocator)
-       self._click_to_element(firstProductDeleteButtonElement)
+        firstProductDeleteButtonElement = self._find_element(self.__firstProductDeleteButtonLocator)
+        self._click_to_element(firstProductDeleteButtonElement)
 
     def save_for_later_first_product_from_cart(self):
         firstProductSaveForLaterButtonElement = self._find_element(self.__firstProductSaveForLaterButtonLocator)
@@ -19,3 +21,15 @@ class CartPage(BasePage):
 
     def deselect_all_items_from_cart(self):
         allItemsDeselectFromCartElement = self._find_element(self.__allItemsDeselectFromCartLocator)
+        self._click_to_element(allItemsDeselectFromCartElement)
+
+    def validate_empty_or_not_cart_page(self):
+        cartCountElement = self._find_element(self.__cartCountLocator)
+        if int(self._get_text(cartCountElement)) == 0:
+            print("Warning! The Cart Is Empty")
+        if int(self._get_text(cartCountElement)) != 0:
+            print("Warning! The Cart Is Empty")
+
+    def get_cart_count_element(self):
+        cartCountElement = self._find_element(self.__cartCountLocator)
+        return int(self._get_text(cartCountElement))
