9da08ee9d08009e2ef2c4fe006cd68a85f68ac13
JenyaKirakosyan27 <147413285+JenyaKirakosyan27@users.noreply.github.com>
2023-11-19 21:07:29 +0400

    Update test_00_06.py

--- tests_/test_00_06.py
+++ tests_/test_00_06.py
@@ -8,7 +8,7 @@ from common_.utilities_.customListener import MyListener
 from selenium.webdriver.support.events import EventFiringWebDriver
 
 
-class ProductDeletion(unittest.TestCase):
+class ProductDeletionTest(unittest.TestCase):
     def setUp(self):
         self.simpleDriver = webdriver.Chrome()
         self.driver = EventFiringWebDriver(self.simpleDriver, MyListener())
@@ -16,29 +16,20 @@ class ProductDeletion(unittest.TestCase):
         self.driver.delete_all_cookies()
         self.driver.maximize_window()
         self.driver.get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
-
-    def test_for_empty_or_not_cart_page(self):
-        loginPageObj = LogInPage(self.driver)
-        loginPageObj.fill_username_field("jenyakirakosyan27@gmail.com")
-        loginPageObj.click_to_continue_button()
-        loginPageObj.fill_password_fild("//eva[@tsaturyan]")
+        self.loginPageObj = LogInPage(self.driver)
+        self.loginPageObj.fill_username_field("jenyakirakosyan27@gmail.com")
+        self.loginPageObj.click_to_continue_button()
+        self.loginPageObj.fill_password_fild("//eva[@tsaturyan]")
         sleep(10)  # Added sleep time to avoid captcha from amazon
-        loginPageObj.click_to_signin_button()
-
+        self.loginPageObj.click_to_signin_button()
+    def test_empty_or_not_cart_page(self):
         navigationBarObj = NavigationBar(self.driver)
         navigationBarObj.click_to_cart_button()
 
         cartPageObj = CartPage(self.driver)
         cartPageObj.validate_empty_or_not_cart_page()
 
-    def test_first_product_deletion(self):
-        loginPageObj = LogInPage(self.driver)
-        loginPageObj.fill_username_field("jenyakirakosyan27@gmail.com")
-        loginPageObj.click_to_continue_button()
-        loginPageObj.fill_password_fild("//eva[@tsaturyan]")
-        sleep(10)  # Added sleep time to avoid captcha from amazon
-        loginPageObj.click_to_signin_button()
-
+    def test_for_first_product_deletion(self):
         navigationBarObj = NavigationBar(self.driver)
         navigationBarObj.click_to_cart_button()
 
@@ -49,21 +40,13 @@ class ProductDeletion(unittest.TestCase):
         self.assertEqual(cartElementBeforeDeletion - 1, cartElementAfterDeletion)
 
     def test_delete_all_products_from_cart(self):
-        loginPageObj = LogInPage(self.driver)
-        loginPageObj.fill_username_field("jenyakirakosyan27@gmail.com")
-        loginPageObj.click_to_continue_button()
-        loginPageObj.fill_password_fild("//eva[@tsaturyan]")
-        sleep(10)  # Added sleep time to avoid captcha from amazon
-        loginPageObj.click_to_signin_button()
-
         navigationBarObj = NavigationBar(self.driver)
         navigationBarObj.click_to_cart_button()
 
         cartPageObj = CartPage(self.driver)
         cartCurrentElement = cartPageObj.get_cart_count_element()
-        while cartCurrentElement != 0:
-            cartPageObj.delete_first_product_from_cart()
-            cartCurrentElement -= 1
-    
+        cartPageObj.delete_all_product_from_cart()
+        self.assertEqual(cartCurrentElement, 0, "The Cart Is Empty")
+
     def tearDown(self):
         self.driver.close()
